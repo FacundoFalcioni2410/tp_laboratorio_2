9fc605dae87f1c11b947c0fd4b81b07cee68c16b
facundofalcioni2410 <facundofalcioni2410@gmail.com>
2020-11-22 14:18:43 -0300

    TP4

--- Trabajo Practico 4/Falcioni.Facundo.2A.TP4/WindowsForms/FrmPantalla.Designer.cs
+++ Trabajo Practico 4/Falcioni.Facundo.2A.TP4/WindowsForms/FrmPantalla.Designer.cs
@@ -191,6 +191,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Teal;
             this.ClientSize = new System.Drawing.Size(221, 424);
+            this.ControlBox = false;
             this.Controls.Add(this.comboBoxResolucion);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
